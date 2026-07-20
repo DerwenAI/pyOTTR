757c27e2321ae94dea9d5aaea5350a8ce5e1a1d3
Paco Nathan <paco@derwen.ai>
2026-07-20 10:40:51 -0700

    show a demo

--- demo.py
+++ demo.py
@@ -4,16 +4,16 @@ import ottr
 # load a simple OTTR template definition
 
 template: str = """
-  @prefix ex: <http://example.org#>.
+@prefix ex: <http://example.org#> .
 
-  ex:FirstName [ottr:IRI ?uri, ?firstName] :: {
-    ottr:Triple (?uri, foaf:firstName, ?firstName )
-  } .
+ex:FirstName [ ottr:IRI ?uri, ?firstName ] :: {
+  ottr:Triple ( ?uri, foaf:firstName, ?firstName )
+} .
 
-  ex:Person[ ?firstName ] :: {
-    ottr:Triple (_:person, rdf:type, foaf:Person ),
-    ex:FirstName (_:person, ?firstName)
-  } .
+ex:Person[ ?firstName ] :: {
+  ottr:Triple ( _:person, rdf:type, foaf:Person ),
+  ex:FirstName ( _:person, ?firstName )
+} .
 """.strip()
 
 generator: ottr.OttrGenerator = ottr.OttrGenerator()
@@ -21,16 +21,16 @@ generator.load_templates(template)
 
 # parse and prepare an instance for execution
 
-inst: str = """
-@prefix ex: <http://example.org#>.
+rdf_data: str = """
+@prefix ex: <http://example.org#> .
 
-ex:Person("Ann").
+ex:Person("Ann") .
 """.strip()
+
+instances: ottr.generator.OttrInstances = generator.instanciate(rdf_data)
 
 # execute the instance, which yield RDF triples
 # the following prints (_:person0, rdf:type, foaf:Person) and (_:person0, foaf:firstName, "Ann")
-
-instances: ottr.generator.OttrInstances = generator.instanciate(inst)
 
 for s, p, o in instances.execute(as_nt = True):
     print("# ----- RDF triple ----- #")
